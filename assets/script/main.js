f70db30f7dd483d159b1ffba9f3efcbcd599f09a
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-14 16:20:56 +0200

    [upd] objects

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,4 +2,41 @@
 
 console.log('sono connesso');
 
+//oggett
 
+const ceo = {
+    name: 'Wayne Barnett',
+    role: 'Ceo',
+    image: './assets/images/wayne-barnett-founder-ceo.jpg'
+
+}
+
+const chiefEditor = {
+    name : 'Angela Caroll',
+    role: 'Chief Editor',
+    image: './assets/images/angela-caroll-chief-editor.jpg'
+}
+
+const officeManager = {
+    name: 'Walter Gordon';
+    role: 'Office Manager',
+    image: './assets/images/walter-gordon-office-manager.jpg'
+}
+
+const socialMediaManager = {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: './assets/images/angela-lopez-social-media-manager.jpg'
+}
+
+const developer = {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image:'./assets/images/scott-estrada-developer.jpg'
+}
+
+const graphicDesigner = {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: './assets/images/barbara-ramos-graphic-designer.jpg'
+}
